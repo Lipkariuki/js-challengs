aa8315a6a4eb0305828d0e967ba12973e75b20ed
Philip Kariuki <lipkariuki@gmail.com>
2023-09-18 23:38:03 +0300

    lates version

---
+++ area_perimeters.js
@@ -0,0 +1,14 @@
+//area of a rectangle
+let height = 6
+let width = 5
+let area = height*width
+console.log(area)
+
+//perimeter of a rectangle
+let perimeterLength = 7
+let perimieterWidth = 9
+let perimeter = 2 * (perimeterLength + perimieterWidth)
+console.log(perimeter)
+//perimeter of a circle
+//let radius = 7
+
